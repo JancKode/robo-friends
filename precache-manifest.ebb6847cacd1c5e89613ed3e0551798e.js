e3fa06289e39bef4237d6e0cd07aba49c9ff9bb3
jacnkode <gyntlify@gmail.com>
2019-07-15 10:37:54 +0800

    Updates

---
+++ precache-manifest.ebb6847cacd1c5e89613ed3e0551798e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22120a4cc2f3970d443eddfc5b4fdc3",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "8fd54e3700ab6c5f2bc9",
+    "url": "/robo-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6e0a22292e64a61595cb",
+    "url": "/robo-friends/static/css/main.a706d244.chunk.css"
+  },
+  {
+    "revision": "8fd54e3700ab6c5f2bc9",
+    "url": "/robo-friends/static/js/2.26e48f30.chunk.js"
+  },
+  {
+    "revision": "6e0a22292e64a61595cb",
+    "url": "/robo-friends/static/js/main.8d822950.chunk.js"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
